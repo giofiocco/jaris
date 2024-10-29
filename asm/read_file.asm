2b018fe8e6450ead8729848f01dad9b625374cf8
giofiocco <gio.fiocco2@gmail.com>
2024-10-29 19:06:26 +0100

    fix

--- asm/read_file.asm
+++ asm/read_file.asm
@@ -9,42 +9,30 @@ GLOBAL read_u16
 -- read a u8 from the file
 -- [0xFFFF, _] if EOF
 read_u8:
-  A_B INCA INCA PUSHA 
-  -- ^ &ndx [_, file]
-  rB_A A_SEC
-  PEEKB rB_AL A_NDX PUSHA 
-  B_A INCA A_B rB_AL POPB
-  -- ^ &ndx [max_ndx, ndx]
-  SUB DECA JMPRZ $return_null -- if (ndx - max_ndx - 1 == 0) goto return_null
-  -- ^ &ndx [_, ndx]
-  MEM_A PUSHA
+  A_B rB_A A_SEC
+  INCB INCB rB_AL PUSHB PUSHA
+  -- ^ ndx &ndx
+  INCB rB_AL
+  POPB SUB JMPRZ $next_file -- if next_char == max_char
+  B_A A_NDX MEM_A PUSHA -- [char, ndx]
+  -- ^ char &ndx
   PUSHB
-  -- ^ ndx *mem &ndx 
-  PEEKAR 0x06 A_B POPA INCA 
-  -- ^ *mem &ndx [ndx+1, &ndx]
-  AL_rB -- ndx += 1
+  -- ^ ndx char &ndx
+  PEEKAR 0x06 A_B POPA INCA AL_rB -- *ndx = ndx + 1
 
   POPA INCSP RET
 
-return_null:
-  -- ^ &ndx
-  RAM_NDX next_sec_index MEM_A INCNDX MEM_AH 
-  INCA JMPRNZ $next_file
-  JMPR $eof
+next_file: -- next_char == max_char
+  -- ^ &ndx [_, ndx]
+  B_A A_NDX MEM_A PUSHA
+  -- ^ char &ndx
+  RAM_NDX next_sec_index MEM_A INCNDX MEM_AH
+  INCA JMPRZ $eof DECA A_SEC -- if next_sec == 0xFFFF goto eof
+  PEEKAR 0x04 DECA DECA A_B SEC_A A_rB -- set file->sec
+  INCB INCB RAM_AL 0x04 AL_rB -- file->ndx = 4
+  INCB RAM_NDX max_ndx_index MEM_A AL_rB -- set file->max_ndx
 
-next_file:
-  -- &ndx [next_sec + 1, _]
-  DECA A_SEC
-  RAM_NDX max_ndx_index 
-
-  RAM_AL data_start POPB AL_rB
-  B_A INCA A_B
-  MEM_A AL_rB
-  RAM_AL 0x03 SUB A_B
-  SEC_A A_rB
-  -- ^ [_, &file]
-
-  B_A JMPR $read_u8
+  POPA INCSP RET
 
 -- [FILE *file, _] -> [char, _]
 -- read a u16 from the file
