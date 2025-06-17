3e43d9daae99a4a4100b8457586b5b5e16bfeec6
giofiocco <gio.fiocco2@gmail.com>
2025-06-17 15:01:42 +0200

    brainfuck

--- asm/brainfuck.asm
+++ asm/brainfuck.asm
@@ -52,51 +52,78 @@ loop:
 
   RAM_AL "+" SUB JMPRNZ $_01
   PEEKAR 0x04 A_B rB_AL INCA AL_rB
-  JMPR $nextchar
+  JMPR $next_char
 _01:
   RAM_AL "-" SUB JMPRNZ $_02
   PEEKAR 0x04 A_B rB_AL DECA AL_rB
-  JMPR $nextchar
+  JMPR $next_char
 _02:
   RAM_AL ">" SUB JMPRNZ $_03
   PEEKAR 0x04 INCA PUSHAR 0x04
   CALL boundcheck
-  JMPR $nextchar
+  JMPR $next_char
 _03:
   RAM_AL "<" SUB JMPRNZ $_04
   PEEKAR 0x04 DECA PUSHAR 0x04
   CALL boundcheck
-  JMPR $nextchar
+  JMPR $next_char
 _04:
   RAM_AL "[" SUB JMPRNZ $_05
-  PEEKAR 0x04 A_B rB_AL CMPA JMPRNZ $nextchar
-  POPA INCA
-searchclosepar:
-  PUSHA
-  A_B rB_AL CMPA JMPRZ $noclose_error
-  RAM_BL "]" SUB JMPRZ $nextchar
-  POPA INCA JMPR $searchclosepar
+  -- ^ prg ptr
+  PEEKAR 0x04 A_B rB_AL CMPA JMPRNZ $next_char
+
+  POPB RAM_AL 0x00 PUSHA PUSHB
+search_close_par:
+  -- ^ prg parcount ptr
+  PEEKB rB_AL A_B
+  CMPA JMPRZ $noclose_error
+  RAM_AL "[" SUB JMPRNZ $__01
+  PEEKAR 0x04 INCA PUSHAR 0x04
+  JMPR $__02
+__01:
+  RAM_AL "]" SUB JMPRNZ $__02
+  PEEKAR 0x04 DECA JMPRZ $found_close_par PUSHAR 0x04
+__02:
+  POPA INCA PUSHA JMPR $search_close_par
+
+found_close_par:
+  POPA INCSP PUSHA
+  JMPR $next_char
+
 _05:
   RAM_AL "]" SUB JMPRNZ $_06
-  PEEKAR 0x04 A_B rB_AL CMPA JMPRZ $nextchar
-  POPA DECA
-searchopenpar:
-  PUSHA
-  A_B rB_AL CMPA JMPRZ $noopen_error
-  RAM_BL "[" SUB JMPRZ $nextchar
-  POPA DECA JMPR $searchopenpar
+  PEEKAR 0x04 A_B rB_AL CMPA JMPRZ $next_char
+
+  POPB RAM_AL 0x00 PUSHA PUSHB
+search_open_par:
+  -- ^ prg parcount ptr
+  PEEKB rB_AL A_B
+  CMPA JMPRZ $noopen_error
+  RAM_AL "]" SUB JMPRNZ $__03
+  PEEKAR 0x04 INCA PUSHAR 0x04
+  JMPR $__04
+__03:
+  RAM_AL "[" SUB JMPRNZ $__04
+  PEEKAR 0x04 DECA JMPRZ $found_open_par PUSHAR 0x04
+__04:
+  POPA DECA PUSHA JMPR $search_open_par
+
+found_open_par:
+  POPA INCSP PUSHA
+  JMPR $next_char
+
 _06:
   RAM_AL "." SUB JMPRNZ $_07
   PEEKAR 0x04 A_B rB_AL CALL put_char
-  JMPR $nextchar
+  JMPR $next_char
 _07:
   RAM_AL "," SUB JMPRNZ $_08
   CALL get_char PUSHA
   PEEKAR 0x06 A_B POPA AL_rB
-  JMPR $nextchar
+  JMPR $next_char
 _08:
 
-nextchar:
+next_char:
   -- CALLR $print_mem
   POPA INCA PUSHA JMPR $loop
 
