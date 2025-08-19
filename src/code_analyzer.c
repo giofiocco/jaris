1fbab84b4e780f49cf04c932606125a3ed430807
giofiocco <gio.fiocco2@gmail.com>
2025-08-19 18:43:18 +0200

    code_analyzer produce correct graph

--- src/code_analyzer.c
+++ src/code_analyzer.c
@@ -8,20 +8,67 @@
 #include "../argparse.h"
 #include "../mystb/errors.h"
 
-typedef struct node_t_ {
-  int index;
+typedef struct {
   int sp;
   bytecode_t bc;
 
-  struct node_t_ *next;
-  struct node_t_ *branch;
+  int next;
+  int branch;
 } node_t;
 
-node_t *node_malloc(bytecode_t bc) {
-  node_t *node = malloc(sizeof(node_t));
-  assert(node);
-  *node = (node_t){-1, -1, bc, NULL, NULL};
-  return node;
+#define MAX_NODE_COUNT  1028
+#define MAX_LABEL_COUNT 128
+typedef struct {
+  node_t nodes[MAX_NODE_COUNT];
+  int node_count;
+  char labels[MAX_LABEL_COUNT][LABEL_MAX_LEN];
+  int labels_node[MAX_LABEL_COUNT];
+  int label_count;
+} context_t;
+
+int search_label(context_t *context, char *label) {
+  assert(context);
+  assert(label);
+
+  for (int i = 0; i < context->label_count; ++i) {
+    if (strcmp(context->labels[i], label) == 0) {
+      assert(context->labels_node[i]);
+      return context->labels_node[i];
+      break;
+    }
+  }
+  assert(0 && "label not found");
+}
+
+void path(context_t *context, int starting_node) {
+  assert(context);
+
+  for (int i = starting_node; context->nodes[i].next == -1 && i < context->node_count;) {
+    node_t *node = &context->nodes[i];
+
+    if (node->bc.kind == BINSTLABEL && node->bc.inst == CALL && strcmp(node->bc.arg.string, "exit") == 0) {
+      return;
+    } else if ((node->bc.kind == BINSTLABEL || node->bc.kind == BINSTRELLABEL) && (node->bc.inst == JMP || node->bc.inst == JMPR)) {
+      int n = search_label(context, node->bc.arg.string);
+      node->next = n;
+      i = n;
+    } else if (node->bc.kind == BINSTRELLABEL
+               && (node->bc.inst == JMPRZ
+                   || node->bc.inst == JMPRN
+                   || node->bc.inst == JMPRC
+                   || node->bc.inst == JMPRNZ
+                   || node->bc.inst == JMPRNN
+                   || node->bc.inst == JMPRNC)) {
+      int n = search_label(context, node->bc.arg.string);
+      node->branch = n;
+      path(context, n);
+      node->next = i + 1;
+      i += 1;
+    } else {
+      node->next = i + 1;
+      i += 1;
+    }
+  }
 }
 
 void print_help() {
@@ -69,72 +116,50 @@ int main(int argc, char **argv) {
   asm_tokenizer_t tok = {0};
   asm_tokenizer_init(&tok, buffer, input, 0);
 
-  node_t nodes[1028] = {0};
-  int node_count = 0;
-
-  char labels[128][LABEL_MAX_LEN] = {0};
-  int labels_node[128] = {0};
-  int label_count = 0;
+  context_t context = {0};
 
   bytecode_t bc = {0};
   while ((bc = asm_parse_bytecode(&tok)).kind != BNONE) {
-    nodes[node_count++] = (node_t){-1, -1, bc, NULL, NULL};
+    assert(context.node_count + 1 < MAX_NODE_COUNT);
+    context.nodes[context.node_count++] = (node_t){-1, bc, -1, -1};
 
     if (bc.kind == BSETLABEL) {
-      assert(label_count + 1 < 128);
-      strcpy(labels[label_count], bc.arg.string);
-      labels_node[label_count] = node_count - 1;
-      label_count++;
+      assert(context.label_count + 1 < MAX_LABEL_COUNT);
+      strncpy(context.labels[context.label_count], bc.arg.string, LABEL_MAX_LEN);
+      context.labels_node[context.label_count] = context.node_count - 1;
+      context.label_count++;
     }
   }
 
-  for (int i = 0; i < node_count; ++i) {
-    node_t *node = &nodes[i];
-
-    if ((node->bc.kind == BINSTLABEL
-         || node->bc.kind == BINSTRELLABEL)
-        && (node->bc.inst == JMP
-            || node->bc.inst == JMPR
-            || node->bc.inst == JMPRZ
-            || node->bc.inst == JMPRN
-            || node->bc.inst == JMPRC
-            || node->bc.inst == JMPRNZ
-            || node->bc.inst == JMPRNN
-            || node->bc.inst == JMPRNC)) {
-      for (int i = 0; i < label_count; ++i) {
-        if (strcmp(labels[i], node->bc.arg.string) == 0) {
-          assert(labels_node[i]);
-          node->branch = &nodes[labels_node[i]];
-          break;
-        }
-      }
-    }
-  }
+  path(&context, 0);
 
   printf("digraph {\n\tnode [shape = box];\n");
 
-  for (int i = 0; i < node_count; ++i) {
-    node_t *node = &nodes[i];
+  for (int i = 0; i < context.node_count; ++i) {
+    node_t *node = &context.nodes[i];
 
     if (node->bc.kind == BNONE || node->bc.kind == BEXTERN || node->bc.kind == BGLOBAL) {
       continue;
     }
-    node->index = i;
-    printf("\tN%03d node [label=", i);
+    printf("\tN%03d [label=", i);
     printf("< ");
     bytecode_to_asm(stdout, node->bc);
     printf(">");
     printf("];\n");
   }
 
-  for (int i = 0; i < node_count; ++i) {
-    node_t *node = &nodes[i];
+  for (int i = 0; i < context.node_count; ++i) {
+    node_t *node = &context.nodes[i];
 
-    if (node->next) {
-      printf("\tN%03d -> N%03d;\n", node->index, node->next->index);
+    if (node->bc.kind == BNONE || node->bc.kind == BEXTERN || node->bc.kind == BGLOBAL) {
+      continue;
     }
-    if (node->branch) {
-      printf("\tN%03d -> N%03d;\n", node->index + 1, node->branch->index + 1);
+
+    if (node->next != -1) {
+      printf("\tN%03d -> N%03d;\n", i, node->next);
+    }
+    if (node->branch != -1) {
+      printf("\tN%03d -> N%03d [label=\"branch\"];\n", i, node->branch);
     }
   }
 
