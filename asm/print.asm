4af01c912a2bff640fd825c1ba84563793a63c2d
giofiocco <gio.fiocco2@gmail.com>
2025-11-10 16:38:38 +0100

    print wrap if rows grows too much clearing the next line on newline

--- asm/print.asm
+++ asm/print.asm
@@ -7,27 +7,15 @@ EXTERN stream_write
 
   { current_process_ptr 0xF802 }
   { stdout_offset 0x06 }
-  { max_cols 0x4A }
+  { max_cols 0x4F }
+  { max_rows 0x2C }
 
 ALIGN
 pos_ptr:
 col_ptr: 0x00
 row_ptr: 0x00
 
--- [cstr ptr, _] -> [_, _]
-print:
-  PUSHA A_B rB_AL
-  CMPA JMPRZ $print_end
-  CALL put_char
-  POPA INCA
-  JMPR $print
-
-print_end:
-  -- ^ ptr
-  INCSP RET
-
 -- [char, _] -> [_, _]
--- TODO: wrap screen if row too low
 put_char:
   PUSHA
 
@@ -36,22 +24,40 @@ put_char:
   -- ^ char [stdout_ptr + 1, _]
   DECA POPB CALL stream_write RET
 no_redirect:
-
   PEEKA RAM_BL 0x0A SUB JMPRNZ $no_new_line
-
-  RAM_B col_ptr RAM_AL 0x00 AL_rB
-  RAM_B row_ptr rB_AL INCA AL_rB
-  INCSP RET
+  -- ^ char
+  INCSP JMPR $new_line
 no_new_line:
   RAM_B pos_ptr rB_A
   A_B POPA DRW
 
   RAM_B col_ptr rB_AL INCA AL_rB
-  RAM_BL max_cols SUB JMPRNN $end_new_line
-  RAM_B col_ptr RAM_AL 0x00 AL_rB
-  RAM_B row_ptr rB_AL INCA AL_rB
-end_new_line:
+  RAM_BL max_cols SUB JMPRN $new_line
   RET
+new_line:
+  RAM_B row_ptr rB_AL INCA AL_rB
+  A_B RAM_AL max_rows SUB JMPRN $end_new_line -- if (row < max_rows) goto end_new_line => row - max_rows
+  RAM_B row_ptr RAM_AL 0x00 AL_rB
+end_new_line:
+  RAM_B col_ptr RAM_AL 0x00 AL_rB
+  RAM_AL max_cols
+clear_line:
+  PUSHA
+  -- ^ i
+  RAM_B pos_ptr rB_A A_B RAM_AL 0x00 DRW
+  RAM_B col_ptr rB_AL INCA AL_rB
+  POPA DECA JMPRNZ $clear_line
+  RAM_B col_ptr RAM_AL 0x00 AL_rB
+  RET
+
+-- [cstr ptr, _] -> [_, _]
+print:
+  A_B rB_AL CMPA JMPRNZ $not_null
+  RET
+not_null:
+  PUSHB
+  CALL put_char
+  POPA INCA JMPR $print
 
 -- [u16 num, _] -> [_, _]
 -- crash [0xEFFA, _] if stdout is full
